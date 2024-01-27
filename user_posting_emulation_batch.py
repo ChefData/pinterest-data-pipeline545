bed73182743df2a2524a47b07cb415faa1d20c1b
Nick Armstrong <150172835+ChefData@users.noreply.github.com>
2024-01-27 17:13:37 +0000

    Update user_posting_emulation_batch.py

--- user_posting_emulation_batch.py
+++ user_posting_emulation_batch.py
@@ -1,20 +1,26 @@
 from classes.aws_db_connector import AWSDBConnector
 import logging
 
+# Configure the logging module
+log_level: int = logging.INFO
+logging.basicConfig(level=log_level)
 
 if __name__ == "__main__":
     try:
-        # Configure logging and start the data processing loop
-        logging.basicConfig(level=logging.INFO)
-        topics_dict = {'pin': 'pinterest_data', 'geo': 'geolocation_data', 'user': 'user_data'}
-        connector = AWSDBConnector(topics_dict)
+        # Start the data processing loop
+        topics_dict: dict = {'pin': 'pinterest_data', 'geo': 'geolocation_data', 'user': 'user_data'}
+        
         try:
-            print('Working')
-            connector.run_infinite_post_data_loop(streaming = False)
+            connector = AWSDBConnector(topics_dict)
+            logging.info('Working')
+            connector.run_infinite_post_data_loop(streaming=False)
         except KeyboardInterrupt:
             # Gracefully handle KeyboardInterrupt (Ctrl+C)
-            pass
+            logging.info('KeyboardInterrupt: Stopping the data processing loop.')
+        except Exception as connector_error:
+            logging.error(f"Error during AWSDBConnector initialisation: {connector_error}")
         finally:
-            print('Exiting the script')
-    except Exception as e:
-        logging.error(f"Error during main execution: {e}")
+            logging.info('Exiting the script')
+
+    except Exception as main_error:
+        logging.error(f"Error during main execution: {main_error}")
