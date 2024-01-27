f3247ff3932f53a6809eb29ba8983c734ee7f1d8
Nick Armstrong <150172835+ChefData@users.noreply.github.com>
2024-01-27 16:40:55 +0000

    Update aws_db_connector.py

--- classes/aws_db_connector.py
+++ classes/aws_db_connector.py
@@ -2,6 +2,7 @@ from classes.api_communicator import APICommunicator
 from classes.rds_db_connector import RDSDBConnector
 from sqlalchemy import text, bindparam
 from time import sleep
+from typing import Dict, Union
 import logging, signal, random
 
 
@@ -9,29 +10,43 @@ import logging, signal, random
 random.seed(100)
 
 # Configure the logging module
-log_level = logging.INFO
+log_level: int = logging.INFO
 logging.basicConfig(level=log_level)
 
 class AWSDBConnector:
     """Class for connecting to a database and sending data to an API."""
 
-    def __init__(self, topics_dict: dict) -> None:
-        """Initialize the database and API connections."""
-        # Initialize connections
-        self.database_connector = RDSDBConnector()
-        self.api_communicator = APICommunicator()
-        # Register a signal handler for graceful termination
-        self.stop_flag: bool = False
-        self.topics_dict: dict = topics_dict
-        signal.signal(signal.SIGINT, self.__handle_signal)
-        
-    def __handle_signal(self, signum, frame) -> None:
-        """Handle the termination signal."""
+    def __init__(self, topics_dict: Dict[str, str]) -> None:
+        """
+        Initialise the database and API connections.
+
+        Parameters:
+        - topics_dict (Dict[str, str]): A dictionary mapping topic names to corresponding database table names.
+        """
+        try:
+            # Initialise connections
+            self.database_connector = RDSDBConnector()
+            self.api_communicator = APICommunicator()
+            # Register a signal handler for graceful termination
+            self.stop_flag: bool = False
+            self.topics_dict: Dict[str, str] = topics_dict
+            signal.signal(signal.SIGINT, self.__handle_signal)
+        except Exception as error:
+            raise RuntimeError(f"Error during initialisation: {error}")
+
+    def __handle_signal(self, signum: int, frame) -> None:
+        """
+        Handle the termination signal.
+
+        Parameters:
+        - signum (int): The signal number.
+        - frame: The current execution frame.
+        """
         logging.info("Received signal to stop. Exiting gracefully.")
         self.stop_flag = True
 
     @staticmethod
-    def __get_random_row(connection, table_name: str, random_row: int) -> dict:
+    def __get_random_row(connection, table_name: str, random_row: int) -> Dict[str, Union[int, str]]:
         """
         Get a random row from the specified table.
 
@@ -53,14 +68,16 @@ class AWSDBConnector:
             logging.error(f"Error during database query: {error}")
             raise
 
-    def _simulate_asynchronous_data_fetching(self) -> None:
+    def __simulate_asynchronous_data_fetching(self) -> None:
         """
         Introduce a random sleep to simulate asynchronous data fetching.
         """
-        #sleep(random.randrange(0, 2))
-        sleep(random.uniform(0, 2))
+        try:
+            sleep(random.uniform(0, 2))
+        except Exception as error:
+            logging.error(f"Error during data fetching simulation: {error}")
 
-    def _process_data(self, streaming: bool) -> None:
+    def __process_data(self, streaming: bool) -> None:
         """
         Fetch random rows from different tables and send data to the API.
 
@@ -68,15 +85,19 @@ class AWSDBConnector:
             streaming (bool): If True, send data to the API in a streaming manner;
                               otherwise, send data in batches.
         """
-        random_row = random.randint(0, 11000)
-        with self.database_connector.db_engine.connect() as connection:
-            for topic, table_name in self.topics_dict.items():
-                result = self.__get_random_row(connection, table_name, random_row)
-                logging.info(f'Random example of {table_name}: %s', result)
-                if streaming:
-                    self.api_communicator._send_data_stream_to_api(topic, [result])
-                else:
-                    self.api_communicator._send_data_batch_to_api(topic, [result])
+        try:
+            random_row = random.randint(0, 11000)
+            with self.database_connector.db_engine.connect() as connection:
+                for topic, table_name in self.topics_dict.items():
+                    result: Dict[str, Union[int, str]] = self.__get_random_row(connection, table_name, random_row)
+                    logging.info(f'Random example of {table_name}: %s', result)
+                    data_to_send = [result]
+                    if streaming:
+                        self.api_communicator._send_data_stream_to_api(topic, data_to_send)
+                    else:
+                        self.api_communicator._send_data_batch_to_api(topic, data_to_send)
+        except Exception as error:
+            logging.error(f"Error during data processing: {error}")
 
     def run_infinite_post_data_loop(self, streaming: bool) -> None:
         """
@@ -88,8 +109,8 @@ class AWSDBConnector:
         """
         while not self.stop_flag:
             try:
-                self._simulate_asynchronous_data_fetching()
-                self._process_data(streaming)
+                self.__simulate_asynchronous_data_fetching()
+                self.__process_data(streaming)
             except Exception as error:
                 logging.error(f"Unexpected error: {error}")
 
